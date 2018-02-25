10557b0d7017177857e69b06827561e6f7ad89c7
Unknown <danoh82@gmail.com>
2018-02-24 21:09:00 -0800

    Auto stash before merge of "master" and "origin/master"

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -13,39 +13,38 @@ class ContactForm extends Component {
     state = {}
 
     handleChange = (e, { value }) => this.setState({ value })
-    
+
     render() {
         return (
             <Segment style={{ padding: '8em 0em', minHeight: '800px' }} vertical>
                 <Container>
-                    <Form>
-                        <Grid container stackable verticalAlign='middle'>
+                    <Grid container stackable verticalAlign='middle'>
+                        <Form style={{ width: '80%', margin: '20px auto'}}>
                             <Grid.Row>
                                 <Grid.Column width={16}>
-                                    <Form.Input fluid label='First name' placeholder='First name' style={{ width: '100%', margin: '15px 0' }} />
-                                    <Form.Input fluid label='Last name' placeholder='Last name' style={{ width: '100%', margin: '15px 0' }} />
-                                    <Form.Input fluid label='Email' placeholder='Email' style={{ width: '100%', margin: '15px 0' }} />
+                                    <Form.Input fluid required label='First name' placeholder='First name' style={{ width: '100%', margin: '15px 0' }} />
+                                    <Form.Input fluid required label='Last name' placeholder='Last name' style={{ width: '100%', margin: '15px 0' }} />
+                                    <Form.Input fluid required label='Email' placeholder='Email' style={{ width: '100%', margin: '15px 0' }} />
                                 </Grid.Column>
-                                <Grid.Column width={16} style={{ maxHeight: '100%',   }}>
-                                    <TextArea placeholder='Leave a Message' style={{ margin: '0px 0px 50px 0px'}} />
-                                    <Form.Button style={{width: '100%', maxWidth: '100%'}}>Submit</Form.Button>
-                                </Grid.Column>
-                            </Grid.Row>
-                            <Grid.Row style={{margin: '50px 0px'}}>
-                                <Grid.Column width={4} style={{ textAlign: 'center' }}>
-                                    <Icon name='github' size='huge' />
-                                </Grid.Column>
-                                <Grid.Column width={8} style={{ textAlign: 'center' }}>
-                                    <Icon name='linkedin' size='huge' />
-                                </Grid.Column>
-                                <Grid.Column width={4} style={{ textAlign: 'center' }}>
-                                    <Icon name='gitlab' size='huge' />
+                                <Grid.Column width={16} style={{ maxHeight: '100%', }}>
+                                    <Form.TextArea placeholder='Leave a Message' style={{ margin: '0px 0px 50px 0px' }} />
+                                    <Form.Button type='submit' style={{ width: '100%', maxWidth: '100%' }}>Submit</Form.Button>
                                 </Grid.Column>
                             </Grid.Row>
-                        </Grid>
-                    </Form>
+                        </Form>
+                        <Grid.Row style={{ margin: '50px 0px' }}>
+                            <Grid.Column width={4} style={{ textAlign: 'center' }}>
+                                <Icon name='github' size='huge' />
+                            </Grid.Column>
+                            <Grid.Column width={8} style={{ textAlign: 'center' }}>
+                                <Icon name='linkedin' size='huge' />
+                            </Grid.Column>
+                            <Grid.Column width={4} style={{ textAlign: 'center' }}>
+                                <Icon name='gitlab' size='huge' />
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Container>
-
             </Segment>
 
         )
