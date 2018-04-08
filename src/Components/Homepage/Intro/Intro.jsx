1067968c89ce57e9aac77f252b8290c8e96da581
Dan Oh <danoh82@gmail.com>
2018-04-08 09:25:10 -0700

    added Logos animation WIP

--- src/Components/Homepage/Intro/Intro.jsx
+++ src/Components/Homepage/Intro/Intro.jsx
@@ -12,7 +12,21 @@ import {
 } from 'semantic-ui-react';
 import codingLogo from '../../../backgroundImage/technology.svg';
 import './Intro.css';
-
+import reactLogo from './Logos/logo.svg';
+import bootstrapLogo from './Logos/bootstrap-icon.svg';
+import ejsLogo from './Logos/EJS.png'
+import ES6 from './Logos/es6.png';
+import expressJsLogo from './Logos/expressjslogo.png';
+import handlebars from './Logos/handlebars.svg';
+import htmlLogo from './Logos/html.svg';
+import cssLogo from './Logos/css.svg';
+import materialUiLogo from './Logos/material-ui.svg';
+import mongoDBLogo from './Logos/mongoDB.png';
+import mySqlLogo from './Logos/mysql-5.svg';
+import nodejsLogo from './Logos/nodejs.svg';
+import reactNatLogo from './Logos/reactnativelogo.png';
+import semanticLogo from './Logos/semantic_UI.png';
+import sequelizeLogo from './Logos/sequelize.svg';
 
 
 export default class Intro extends Component {
@@ -39,14 +53,14 @@ export default class Intro extends Component {
                                 <div className='Intro'>
                                     <Header as='h3' style={{ fontSize: '2em' }}>Who am I?</Header>
                                     <p style={{ fontSize: '1.33em' }}>
-                                        <b>Hello there?!</b> First, Welcome to my website. I'm a full stack web developer based in Los Angeles, CA, and while I have a wide knowledge base of many languages, frameworks and libraries, I enjoying working in the MERN stack environment. 
-                                        Graduate of UC Irvine's full stack development boot camp program and am eager to use skills acquired, while constantly learning more. Have experience in Google's EWOQ and vehicle multimedia (electrical) engineering at Hyundai. 
+                                        <b>Hello there?!</b> First, Welcome to my website. I'm a full stack web developer based in Los Angeles, CA, and while I have a wide knowledge base of many languages, frameworks and libraries, I enjoying working in the MERN stack environment.
+                                        Graduate of UC Irvine's full stack development boot camp program and am eager to use skills acquired, while constantly learning more. Have experience in Google's EWOQ and vehicle multimedia (electrical) engineering at Hyundai.
                                     </p>
                                 </div>
                                 <div className='Intro'>
                                     <Header as='h3' style={{ fontSize: '2em' }}>Web Dev Skills</Header>
                                     <div style={{ fontSize: '1.33em' }}>
-                                        <div className='IntroP'>Experienced in : </div> 
+                                        <div className='IntroP'>Experienced in : </div>
                                         <div className='IntroSkills'>HTML, CSS (Less/Sass), JavaScript(ES5, ES6), jQuery, Bootstrap, MERN stack (MongoDB, Express, React, Node), MySQL, Sequelize, Material-UI, Sematic-UI, NPM, GIT, Paypal</div>
                                         <div className='IntroP'>Currently learning :</div>
                                         <div className='IntroSkills'> Angular, ES7, Redux, Vue, AWS </div>
@@ -54,14 +68,69 @@ export default class Intro extends Component {
                                 </div>
                             </Grid.Column>
                         </Grid.Row>
-                        <Grid.Row>
-                            <img src={codingLogo} alt="codingLogo" style={{height: '300px', width: '100%'}}/>
-                        </Grid.Row>
-                        <Grid.Row style={{paddingTop: '7rem'}}>
+                        <Grid.Row style={{ margin: '50px auto' }}>
                             <Grid.Column textAlign='center'>
                                 <Button size='huge'><a href='#Portfolio'>WebDev Portfolio</a></Button>
                             </Grid.Column>
                         </Grid.Row>
+                        <Container>
+                            <Grid.Row style={{ textAlign: 'center', marginTop: '50px' }}>
+                                <Image.Group className='TechUsedDiv'>
+                                    <div className='TechCard'>
+                                        <Image src={reactLogo} size='small' className='front' verticalAlign='middle'/>
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={bootstrapLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={ejsLogo} size='small' className='front' />
+                                        <div className='back'>
+                                            <div className="details">
+                                                Framework Detail
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={ES6} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={handlebars} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={htmlLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={cssLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={materialUiLogo} size='small' className='front' />
+                                    </div>
+                                </Image.Group>
+                                <Image.Group className='TechUsedDiv'>
+                                <div className='TechCard'>
+                                        <Image src={mongoDBLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={mySqlLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={nodejsLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={reactNatLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={semanticLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={sequelizeLogo} size='small' className='front' />
+                                    </div>
+                                    <div className='TechCard'>
+                                        <Image src={expressJsLogo} size='small' className='front' />
+                                    </div>
+                                </Image.Group>
+                            </Grid.Row>
+                        </Container>
                     </Grid>
                 </Segment>
 
--- src/Components/Homepage/Portfolio/Portfolio.jsx
+++ src/Components/Homepage/Portfolio/Portfolio.jsx
@@ -12,21 +12,7 @@ import Port2 from './Images/burger.png';
 import Port3 from './Images/triviaGame.png';
 import Port4 from './Images/TechCheck.png';
 import './Portfolio.css';
-import reactLogo from './Images/Logos/logo.svg';
-import bootstrapLogo from './Images/Logos/bootstrap-icon.svg';
-import ejsLogo from './Images/Logos/EJS.png'
-import ES6 from './Images/Logos/es6.png';
-import expressJsLogo from './Images/Logos/expressjslogo.png';
-import handlebars from './Images/Logos/handlebars.svg';
-import htmlLogo from './Images/Logos/html.svg';
-import cssLogo from './Images/Logos/css.svg';
-import materialUiLogo from './Images/Logos/material-ui.svg';
-import mongoDBLogo from './Images/Logos/mongoDB.png';
-import mySqlLogo from './Images/Logos/mysql-5.svg';
-import nodejsLogo from './Images/Logos/nodejs.svg';
-import reactNatLogo from './Images/Logos/reactnativelogo.png';
-import semanticLogo from './Images/Logos/semantic_UI.png';
-import sequelizeLogo from './Images/Logos/sequelize.svg';
+
 
 class portfolio extends Component {
     render() {
@@ -35,21 +21,21 @@ class portfolio extends Component {
                 <Segment style={{ padding: '8em 0em', textAlign: 'center', backgroundColor: '#D3D3D3' }} vertical>
                     <Container >
                         <Grid>
-                            <Grid.Column computer={8} widescreen={8} tablet={8} largeScreen={8} mobile={16}>
+                        <Grid.Column computer={8} widescreen={8} tablet={8} largeScreen={8} mobile={16}>
                                     <div className="PortContainer"
-                                        style={{ backgroundImage: `url(${Port1})` }} >
+                                        style={{ backgroundImage: `url(${Port3})` }} >
                                         <div className="textOverImage">
-                                            <h2>Simpsons Game</h2>
-                                                <h3>Made with :  </h3>
-                                                <p>JavaScript(ES6), NodeJS, ReactJS</p>  
-                                                <h3>Links :</h3>                                                                             
+                                            <h2>The Quiz Show Game</h2>
+                                            <h3>Made with :  </h3>
+                                                <p>HTML, CSS, JavaScript, jQuery</p> 
+                                            <h3>Links :</h3>                                                                             
                                             <Grid column={2}>
                                                 <Grid.Column width={8}>                                                 
-                                                <a href='https://github.com/danniboi82/clicky-game' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='github' size='big' link></Icon></a>                                  
+                                                <a href='https://github.com/danniboi82/TriviaGame' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='github' size='big' link></Icon></a>                                  
                                                     <p>Github</p>
                                                 </Grid.Column>
                                                 <Grid.Column width={8}>
-                                                    <a href='https://danoh82-clicky.herokuapp.com/' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='tv' size='big' link></Icon></a>                                  
+                                                    <a href='https://danoh82-triviagame.herokuapp.com/index.html' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='tv' size='big' link></Icon></a>                                  
                                                     <p>Live Site</p>
                                                 </Grid.Column>
                                             </Grid>
@@ -79,19 +65,19 @@ class portfolio extends Component {
                             </Grid.Column>
                             <Grid.Column computer={8} widescreen={8} tablet={8} largeScreen={8} mobile={16}>
                                     <div className="PortContainer"
-                                        style={{ backgroundImage: `url(${Port3})` }} >
+                                        style={{ backgroundImage: `url(${Port1})` }} >
                                         <div className="textOverImage">
-                                            <h2>The Quiz Show Game</h2>
-                                            <h3>Made with :  </h3>
-                                                <p>HTML, CSS, JavaScript, jQuery</p> 
-                                            <h3>Links :</h3>                                                                             
+                                            <h2>Simpsons Game</h2>
+                                                <h3>Made with :  </h3>
+                                                <p>JavaScript(ES6), NodeJS, ReactJS</p>  
+                                                <h3>Links :</h3>                                                                             
                                             <Grid column={2}>
                                                 <Grid.Column width={8}>                                                 
-                                                <a href='https://github.com/danniboi82/TriviaGame' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='github' size='big' link></Icon></a>                                  
+                                                <a href='https://github.com/danniboi82/clicky-game' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='github' size='big' link></Icon></a>                                  
                                                     <p>Github</p>
                                                 </Grid.Column>
                                                 <Grid.Column width={8}>
-                                                    <a href='https://danoh82-triviagame.herokuapp.com/index.html' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='tv' size='big' link></Icon></a>                                  
+                                                    <a href='https://danoh82-clicky.herokuapp.com/' target="_blank" rel="noopener noreferrer" className='portIcons' ><Icon name='tv' size='big' link></Icon></a>                                  
                                                     <p>Live Site</p>
                                                 </Grid.Column>
                                             </Grid>
